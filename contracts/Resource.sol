7dbe12b420e1ab1ff398582c2fab4687fa3b1e2b
uma-victor1 <umavictor11@gmail.com>
2022-02-26 01:52:01 +0100

    added contract functionalities

--- contracts/Resource.sol
+++ contracts/Resource.sol
@@ -1,40 +1,71 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;  // Solidity files have to start with this pragma.
+pragma solidity ^0.8.0; // Solidity files have to start with this pragma.
+pragma experimental ABIEncoderV2;
 
-import "hardhat/console.sol";
-
-// This is the where all the fun stuff happens
 contract ResourceShare {
+    struct Resource {
+        uint256 id;
+        address creator;
+        string title;
+        string url;
+        string description;
+        int64 total_votes;
+    }
 
- struct Resource{
-    uint id;
-    address creator;
-    string title;
-    string url;
-    string description;
-    int64 total_votes;
-}
-  
+    mapping(uint256 => Resource) public resources;
+    event savingsEvent(uint256 indexed _resourceId);
+    uint256 public numResource;
 
-  
-Resource[] private resources;
+    constructor() {
+        numResource = 0;
+        addResource(
+            "freecodecamp",
+            "https://www.freecodecamp.org/",
+            "Learn to code"
+        );
+    }
 
- uint numResources;
+    function addResource(
+        string memory title,
+        string memory url,
+        string memory description
+    ) public {
+        Resource storage resource = resources[numResource];
+        resource.creator = msg.sender;
+        resource.total_votes = 0;
+        resources[numResource] = Resource(
+            numResource,
+            resource.creator,
+            title,
+            url,
+            description,
+            resource.total_votes
+        );
+        numResource++;
+    }
 
-  function addResource(string memory title, string memory url, string memory description) public returns (uint resourceID) {
-   resourceID = numResources++;
-   Resource storage resource = resources[resourceID];
-   resource.id = resourceID;
-   resource.creator = msg.sender;
-   resource.title = title;
-   resource.url = url;
-   resource.description = description;
-   resource.total_votes = 0;
-  }
+    //return a particular resource
+    function getResource(uint256 resourceId)
+        public
+        view
+        returns (Resource memory)
+    {
+        return resources[resourceId];
+    }
 
-  function listResource() external view returns(Resource[] memory) {
-     return resources;
-  }
+    //return the array of resources
+    function getResources() public view returns (Resource[] memory) {
+        Resource[] memory id = new Resource[](numResource);
+        for (uint256 i = 0; i < numResource; i++) {
+            Resource storage resource = resources[i];
+            id[i] = resource;
+        }
+        return id;
+    }
 
-  
+    // Vote a resource
+    function voteResource(uint256 resourceId) public {
+        Resource storage resource = resources[resourceId];
+        resource.total_votes++;
+    }
 }
